7b8ec04d40ef76aa626877d3daadaabece880d62
Gr4nty <Granty@Granty-MacBook-Pro.local>
2013-09-03 18:57:54 +0200

    macbook
    commit with update

--- test.php
+++ test.php
@@ -9,23 +9,24 @@ $url = 'http://localhost/Fastlane/profile/user/id/'.$id.'/format/json';
 
     $handle = curl_init();
     // Set
-    curl_setopt($handle, CURLOPT_URL, $url);
-    curl_setopt($handle, CURLOPT_POST, 1);
     curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($handle, CURLOPT_HTTPHEADER, array('Accept:application/json','Content-type: application/json', 'X-API-KEY: admin'));
+    curl_setopt($handle, CURLOPT_HTTPHEADER, array('X-API-KEY: admin'));
     curl_setopt($handle, CURLOPT_URL, $url);
     $buffer = curl_exec($handle);
 
  
-    var_dump($buffer);
+//    $buffer = json_decode($buffer, true);
+//    $result = file_get_contents($buffer);
+//$buffer = array_values($buffer);
+//  $buffer = json_encode($buffer);  
+  var_dump(get_object_vars($buffer));
   
     
-//$user = json_decode(  
-//    file_get_contents($result)  
-//);  
+$user = json_decode(  
+    file_get_contents($buffer));  
 //
 //echo "Data:</br>";
-//echo 'id=> '; echo $user->id;
+echo 'id=> '; echo $user->id;
 //echo "<br>";
 //echo 'name=> '; echo $user->username;
 //echo "<br>";
