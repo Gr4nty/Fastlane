e4aa47e923af7d81b710053b04f59eacf578158e
Gr4nty <grantys@gmail.com>
2013-09-05 15:40:22 +0200

    Small fix in test.php

--- test.php
+++ test.php
@@ -1,33 +1,24 @@
 <?php
 if (@$_GET["id"] <= null) {
-    $id = 99999999;
+    $id = 1;
 }else{
 $id = $_GET["id"];
 }
-$url = 'http://localhost/Fastlane/profile/user/id/'.$id.'/format/json';
-
+$url = 'http://localhost/Fastlane/view/user/id/'.$id.'/format/json';
+$auth = 'X-API-KEY: admin';
 
     $handle = curl_init();
     // Set
+    curl_setopt($handle, CURLOPT_URL, $url);
     curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($handle, CURLOPT_HTTPHEADER, array('X-API-KEY: admin'));
+    curl_setopt($handle, CURLOPT_HTTPHEADER, array($auth));
     curl_setopt($handle, CURLOPT_URL, $url);
     $buffer = curl_exec($handle);
+    $user = json_decode($buffer);
 
- 
-//    $buffer = json_decode($buffer, true);
-//    $result = file_get_contents($buffer);
-//$buffer = array_values($buffer);
-//  $buffer = json_encode($buffer);  
-  var_dump(get_object_vars($buffer));
-  
-    
-$user = json_decode(  
-    file_get_contents($buffer));  
-//
-//echo "Data:</br>";
+echo "Data:</br>";
 echo 'id=> '; echo $user->id;
-//echo "<br>";
-//echo 'name=> '; echo $user->username;
-//echo "<br>";
-//echo 'password=> '; echo $user->password;
+echo "<br>";
+echo 'name=> '; echo $user->username;
+echo "<br>";
+echo 'password=> '; echo $user->password;
